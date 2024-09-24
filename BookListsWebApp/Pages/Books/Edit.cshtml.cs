da2de0ea582ad32dde60c5b120fe1693aa46caad
Salman Farsi <salman.farsi@3together.us>
2024-09-23 23:31:06 -0400

    Added edit page

---
+++ BookListsWebApp/Pages/Books/Edit.cshtml.cs
@@ -0,0 +1,51 @@
+using BookListsWebApp.Data;
+using BookListsWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListsWebApp.Pages.Books;
+
+public class EditModel : PageModel
+{
+    private readonly BooksDbContext _context;
+
+    public EditModel(BooksDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Book Book { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(Guid? id)
+    {
+        if (id is null)
+        {
+            return RedirectToPage("./Index");
+        }
+
+        var book = await _context.Books.FirstOrDefaultAsync(b => b.Id == id);
+
+        if (book is not null)
+        {
+            Book = book;
+            return Page();
+        }
+
+        return RedirectToPage("./Index");
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        _context.Update(Book);
+        await _context.SaveChangesAsync();
+
+        return RedirectToPage("./Index");
+    }
+}
